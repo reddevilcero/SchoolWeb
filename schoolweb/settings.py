df8c9bcccbeb461422de2adbebf3e2c1128187fc
Pedro David Garcia Lopez <reddevil_cero@hotmail.com>
2019-03-01 12:30:38 +0000

    dev mode

--- schoolweb/settings.py
+++ schoolweb/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4_(2^r8%syn^%4mvq(skqsvdfx#m5qja%!xo&-v6qi2+dbd9j5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['SchoolWebTelford.pythonanywhere.com', 'localhost', '127.0.0.1']
 
