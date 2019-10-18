dad73d0bcfea052d823b50b89e7db78ab27bf99d
reddevilcero <reddevil_cero@hotmail.com>
2019-10-18 10:38:34 +0100

    no

--- core/views.py
+++ core/views.py
@@ -8,13 +8,13 @@ def server_error(request):
     return render(request, 'errors/500.html')
 
 
-def not_found(request):
+def not_found(request,exception):
     return render(request, 'errors/404.html')
 
 
-def permission_denied(request):
+def permission_denied(request,exception):
     return render(request, 'errors/403.html')
 
 
-def bad_request(request):
+def bad_request(request,exception):
     return render(request, 'errors/400.html')
